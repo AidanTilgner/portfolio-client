d9fcebca89a94438856b10f3dfccb25a51d8bbee
Aidan Tilgner <aidantilgner02@gmail.com>
2021-05-25 14:15:31 -0700

    added resume section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 //components
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Porfolio";
+import Resume from "./pages/Resume/Resume";
+import Contact from "./pages/Contact/Contact";
 
 //files
 import "./App.scss";
@@ -14,6 +18,18 @@ function App() {
                 <Redirect exact from="/" to="/home" />
                 <Route path="/home">
                   <Home />
+                </Route>
+                <Route path="/about">
+                    <About/>
+                </Route>
+                <Route path="/portfolio">
+                    <Portfolio/>
+                </Route>
+                <Route path="/resume">
+                    <Resume/>
+                </Route>
+                <Route path="/contact">
+                    <Contact/>
                 </Route>
             </Switch>
         </div>
