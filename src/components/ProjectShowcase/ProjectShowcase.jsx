372c3fa7f904109eaa16544cff278ef155577b59
Aidan Tilgner <aidantilgner02@gmail.com>
2021-06-01 01:45:25 -0700

    made project showcase component

---
+++ src/components/ProjectShowcase/ProjectShowcase.jsx
@@ -0,0 +1,44 @@
+//modules
+import React from 'react'
+
+//files
+import './ProjectShowcase.scss';
+
+function ProjectShowcase({title, description, technologies, previewImage, projectLink}) {
+    return (
+        <div className="project-showcase" id={`${title}`}>
+            <div className="project-showcase__project-preview">
+                <img 
+                    src={
+                            previewImage ? previewImage :
+                            "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+                        }
+                    alt="project-preview-image" 
+                    className="project-showcase__project-preview-image"
+                />
+            </div>
+            <div className="project-showcase__project-details">
+                <h2 className="project-showcase__title">
+                    {title ? title : "title not found"}
+                </h2>
+                <p className="project-showcase__paragraph">
+                    {description ? description : "description not found"}
+                </p>
+                <h3 className="project-showcase__subtitle">
+                    technologies used:
+                </h3>
+                <p className="project-showcase__paragraph">
+                    {technologies ? technologies : "technologies not listed"}
+                </p>
+                <a href={projectLink ? projectLink : "#"} className="project-showcase__link">
+                    <button className="project-showcase__button-outlined">
+                        {projectLink ? "view project" : "link not functional"}
+                    </button>
+                </a>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProjectShowcase
--- src/pages/Portfolio/Porfolio.jsx
+++ src/pages/Portfolio/Porfolio.jsx
@@ -4,23 +4,32 @@ import { Link } from 'react-router-dom';
 
 //components
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import ProjectShowcase from "../../components/ProjectShowcase/ProjectShowcase";
 
 //files
 import "./Portfolio.scss"
 
 function Porfolio() {
     return (
+        <>
         <div className="portfolioPage">
             <Navbar active="portfolio"/>
             <h1 className="portfolioPage__tagline">
-                this area is under construction, give me a couple days :)
+                Here's a taste of what I can do
             </h1>
-            <Link to="/home">
-                    <button className="portfolioPage__button-filled">
-                        return to home
-                    </button>
-            </Link>
+            <ProjectShowcase
+                title={"dummy"}
+                description={"dummy"}
+                technologies={"dummy"}
+                previewImage={false}
+                projectLink={"dummy"}
+            />
+            <ProjectShowcase/>
+            
         </div>
+        <Footer/>
+        </>
     )
 }
 
